12a4c4d66fa17e96e4ed2d5aa71093551744167d
Shawn <61984193+git-shawn@users.noreply.github.com>
2024-11-28 08:42:33 -0700

    Better fonts, open graph data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,30 @@
-import { Inter } from 'next/font/google'
+import { Instrument_Sans, Instrument_Serif } from 'next/font/google'
 import "./globals.css";
 
-const inter = Inter({
-  weight: ["400", "900"],
-  subsets: ['latin'] 
+const instrumentSans = Instrument_Sans({
+  weight: ["500"],
+  subsets: ['latin'],
+  variable: '--instrument-sans'
 })
+
+const instrumentSerif = Instrument_Serif({
+  weight: ["400"],
+  subsets: ['latin'],
+  variable: '--instrument-serif'
+})
+
 
 export const metadata = {
   title: "Shawn",
-  description: "My personal and professional portfolio. Nice to meet you!"
+  description: "My personal and professional portfolio. Nice to meet you!",
+  openGraph: {
+    title: "Shawn",
+    description: "My personal and professional portfolio. Nice to meet you!",
+    url: "www.shawndavis.work",
+    siteName: "Shawn",
+    local: "en_US",
+    type: "website"
+  }
 };
 
 export const viewport = {
@@ -18,7 +34,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${instrumentSans.variable} ${instrumentSerif.variable}`}>
         {children}
       </body>
     </html>
